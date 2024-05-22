18af0e412b1dc08eff159306508f820264ba2acd
Arnold Ema A Ghomfo <2230827@etu.cegepjonquiere.ca>
2024-05-22 12:16:46 -0400

    update final

--- Projet_Boutique_Shoes/boutique_admin/app/loginAdmin/loginSubmit.js
+++ Projet_Boutique_Shoes/boutique_admin/app/loginAdmin/loginSubmit.js
@@ -1,49 +1,93 @@
 "use client";
-import Login from "./login";
+
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
-
 export default function LoginAdmin() {
   const router = useRouter();
-  async function LoginForm(formData) {
-    try {
-      await Login(formData);
-     
-      router.push('../inventaire');
-    } catch (error) {
-       
-      console.error('Erreur lors du login administrateur :', error);
-    }
-  }
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-    return (
-            <div className="container mt-5">
-              <div className="row justify-content-center">
-                <div className="col-md-6">
-                  <div className="card border-0 bg-light shadow">
-                    <div className="card-body p-5">
-                      <h2 className="card-title text-center mb-4">Login</h2>
-                      <form action={LoginForm}>
-                        <div className="form-group contenuLambda">
-                          <label htmlFor="nom">Nom d'utilisateur</label>
-                          <input type="text" className="form-control " id="nom" placeholder="Entrez votre nom d'utilisateur" name="username" />
-                        </div>
-                        <br />
-                        <div className="form-group contenuLambda">
-                          <label htmlFor="motDePasse" >Mot de passe</label>
-                          <input type="password" className="form-control" id="motDePasse" placeholder="Entrez votre mot de passe" name="password" />
-                        </div>
-                        <br />
-                       
-                        <button type="submit" className="btn btn-danger btn-block ml-6">Connexion</button> 
-                        <button type="submit" className="btn btn-danger btn-block  mr-5"><a href="../registerAdmin">Inscription</a></button>
-                      </form>
-                    </div>
-                  </div>
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('Username', username);
+    formData.append('Password', password);
+
+    try {
+      const response = await fetch('https://projet05-dicjprog4.cegepjonquiere.ca/api/authentification/login', {
+        method: 'POST',
+        body: formData
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+       
+        if (data.token) {
+          
+          localStorage.setItem('token', data.token);
+         
+          router.push('../inventaire');
+        } else {
+          setErrorMessage('Nom d\'utilisateur ou mot de passe incorrect. Veuillez réessayer.');
+        }
+      } else {
+        setErrorMessage('Nom d\'utilisateur ou mot de passe incorrect. Veuillez réessayer.');
+      }
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données:', error);
+      setErrorMessage('Erreur lors de la récupération des données.');
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card border-0 bg-light shadow">
+            <div className="card-body p-5">
+              <h2 className="card-title text-center mb-4">Login</h2>
+              <form onSubmit={handleFormSubmit}>
+                <div className="form-group contenuLambda">
+                  <label htmlFor="nom">Nom d'utilisateur</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="nom"
+                    placeholder="Entrez votre nom d'utilisateur"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
                 </div>
-              </div>
+                <br />
+                <div className="form-group contenuLambda">
+                  <label htmlFor="motDePasse">Mot de passe</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="motDePasse"
+                    placeholder="Entrez votre mot de passe"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <br />
+                {errorMessage && <p className="text-danger contenuLambda">{errorMessage}</p>}
+                <button type="submit" className="btn btn-danger btn-block ml-6">Connexion</button>
+                <Link href="../registerAdmin" passHref>
+                  <button type="button" className="btn btn-danger btn-block mr-5">Inscription</button>
+                </Link>
+              </form>
             </div>
-          );
-    
-  }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
